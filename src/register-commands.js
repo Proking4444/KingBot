c462d1456caf246f4d3ba12e4df9f54c1fcd6dd1
Ari <ari.basil.khan@gmail.com>
2024-02-23 17:03:04 -0500

    15th Commit.

--- src/register-commands.js
+++ src/register-commands.js
@@ -15,11 +15,10 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
         console.log('Registering slash commands...');
 
     await rest.put(
-        Routes.applicationGuildCommands(
-            process.env.CLIENT_ID,
-            process.env.GUILD_ID
+        Routes.applicationCommands(
+            process.env.CLIENT_ID
         ),
-      { body: commands }
+      { body: commands },
     );
 
     console.log('Slash commands were registered successfully!');
