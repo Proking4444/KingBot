d940a5237b0d463f29847c1637b580feabfe6117
Ari <ari.basil.khan@gmail.com>
2024-10-23 10:27:47 -0400

    Removed safety settings temporarily.

--- src/index.js
+++ src/index.js
@@ -23,31 +23,8 @@ import { values } from "./constants.js";
 import { HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const safetySettings = [
-  {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-];
-
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const gemini15Flash = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySettings: safetySettings });
+const gemini15Flash = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const gemini15Pro = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
 import { OpenAI } from "openai";
