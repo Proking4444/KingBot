e00ed1e344e545af846d06fa1edcb031d80b3c83
Ari <ari.basil.khan@gmail.com>
2024-10-23 14:22:18 -0400

    Simplified safety settings.

--- src/index.js
+++ src/index.js
@@ -27,30 +27,14 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 const safetySettings = [
   {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY,
+    category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
     threshold: HarmBlockThreshold.BLOCK_NONE,
   },
 ];
 
-const gemini15Flash = genAI.getGenerativeModel({ 
-  model: "gemini-1.5-flash", 
-  safetySettings: safetySettings 
+const gemini15Flash = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  safetySettings: safetySettings,
 });
 
 const gemini15Pro = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
