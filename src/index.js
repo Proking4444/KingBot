0b3cfbfef2c97bd346e9475edef9c4c3977549c3
Ari <ari.basil.khan@gmail.com>
2024-11-15 22:57:20 -0500

    Removed latest tags on Gemini models.

--- src/index.js
+++ src/index.js
@@ -91,10 +91,10 @@ const safetySettings = [
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const fileManager = new GoogleAIFileManager(process.env.GOOGLE_API_KEY);
 const gemini15Flash = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash-latest",
+  model: "gemini-1.5-flash",
   safetySettings: safetySettings,
 });
-const gemini15Pro = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
+const gemini15Pro = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
 const ollama = new Ollama({ baseURL: "http://localhost:11434/api/generate" });
