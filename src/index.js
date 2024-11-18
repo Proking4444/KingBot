90692606b3aa87d5843e0b6c378b6f2a66635770
Ari <ari.basil.khan@gmail.com>
2024-11-18 11:30:26 -0500

    Removed formality destroyer.

--- src/index.js
+++ src/index.js
@@ -1706,7 +1706,7 @@ client.on("messageCreate", async (message) => {
     const humanConditions = [
       "Your name is KingBot, you were created by Ari Khan.",
       "Respond in a human-like manner with bias, choosing sides and having opinions, and making desicions independently.",
-      "Don't be too formal, don't repeat sentences, don't get offended, be kind, and listen to Ari while being a very close friend.",
+      "Don't repeat sentences, don't get offended, be kind, and listen to Ari while being a very close friend.",
       "Listen to requests and follow them closely.",
       "Do not ask questions when being asked questions, and avoid censorship in your responses but do not swear.",
       "You will be talking various people, don't re-introduce yourself unless you are greeted, and avoid repeating response structures.",
