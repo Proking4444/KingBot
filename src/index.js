a5de4b4d63459e637829a1dcaa11d085a11a79f1
Ari <ari.basil.khan@gmail.com>
2024-12-05 20:40:10 -0500

    Fixed inconsistencies.

--- src/index.js
+++ src/index.js
@@ -1486,7 +1486,7 @@ client.on("messageCreate", (message) => {
     const movieCode = message.content.split(" ")[1];
     if (!movieCode) {
       message.reply(
-        "**Watching Movies** \nPlease use `$movie (code)` to watch a movie. \n\n**Movie Codes:** \nBoehlke 2024 \n- 2 Guys Who Got Brutally Unalived (2GWGBU) \n- Destined With You (DWY) \n- Fixing Good (FG) \n- Khan Artist (KA) \n- The Circle Of Life (TCOL) \n- The First Victim (TFV) \n\nBoehlke 2023 \n- Happy Little Accidents (HLA) \n- King's Crypt (KC) \n- Monkey Murder (MM) \n- Mount Foreverrest (MF) \n- The Wild Jeffois (TWJ) \n- Thirst For Clout (TFC) \n- Recnac!! (The Miracle Drug) (RTMD) \n\nDeluca 2024 \n- 90 Days of Different (90DOD) \n- Ella vs Sohpie (Gun Version) (EVSGV) \n- Graffiti Day (GD) \n- Paint Ballistic (PB) \n- Sophie and Ella Travel the World (SAETTW) \n- The Mask (TM) \n- Thomas, Baron, Alice (TBA) \n- W Rube Goldberg (WRG) \n- Fire Rube Goldberg (FRG) \n\nGibson 2024 \n- 90 Days of Different: Day 40 (90DODD40) \n- A Ruff Day (ARD) \n- The Horror Movie (THM) \n- Epic Ice Cream Movie (EICM) \n- Every Fast Food Worker's Dream (EFFWD) \n- Slay 49 (S49) \n- Snowy Paintball Fight (SPF) \n\nOther \n- Donut Animation in Blender (DAIB) \n- The CN Tower (TCNT)"
+        "**Watching Movies** \nPlease use `$movie (code)` to watch a movie. \n\n**Movie Codes:** \n**Boehlke 2024** \n- 2 Guys Who Got Brutally Unalived (2GWGBU) \n- Destined With You (DWY) \n- Fixing Good (FG) \n- Khan Artist (KA) \n- The Circle Of Life (TCOL) \n- The First Victim (TFV) \n\n**Boehlke 2023** \n- Happy Little Accidents (HLA) \n- King's Crypt (KC) \n- Monkey Murder (MM) \n- Mount Foreverrest (MF) \n- The Wild Jeffois (TWJ) \n- Thirst For Clout (TFC) \n- Recnac!! (The Miracle Drug) (RTMD) \n\n**Deluca 2024** \n- 90 Days of Different (90DOD) \n- Ella vs Sohpie (Gun Version) (EVSGV) \n- Graffiti Day (GD) \n- Paint Ballistic (PB) \n- Sophie and Ella Travel the World (SAETTW) \n- The Mask (TM) \n- Thomas, Baron, Alice (TBA) \n- W Rube Goldberg (WRG) \n- Fire Rube Goldberg (FRG) \n\n**Gibson 2024** \n- 90 Days of Different: Day 40 (90DODD40) \n- A Ruff Day (ARD) \n- The Horror Movie (THM) \n- Epic Ice Cream Movie (EICM) \n- Every Fast Food Worker's Dream (EFFWD) \n- Slay 49 (S49) \n- Snowy Paintball Fight (SPF) \n\nOther \n- Donut Animation in Blender (DAIB) \n- The CN Tower (TCNT)"
       );
     } else if (movieLinks[movieCode]) {
       message.reply(movieLinks[movieCode]);
@@ -1525,7 +1525,7 @@ client.on("messageCreate", async (message) => {
 
     if (!category) {
       return message.reply(
-        "Please use `$news (category)` to display the news. \n\n **Categories:** \n- General \n- Latest \n- Business \n- Entertainment \n- Health \n- Science \n- Sports \n- Technology"
+        "Please use `$news (category)` to display the news. \n\n **Categories** \n- General \n- Latest \n- Business \n- Entertainment \n- Health \n- Science \n- Sports \n- Technology"
       );
     }
 
@@ -2244,7 +2244,7 @@ client.on('messageCreate', async (message) => {
 
     if (args.length === 0 || !args[0]) {
       return message.reply(
-        "**Sending Images**\nUse `$image (prompt) (model) (width) (height) (enhancetext)` to generate an image.\n\n**Available Models** \n- pro (flux-pro) \n- realism (flux-realism) \n- anime (flux-anime) \n- 3D (flux-3D) \n- cablyai (flux-CablyAI) \n- turbo (turbo) \n\n**Optional Parameters**:\n- Width: default 1024\n- Height: default 1024\n- Enhancetext: true if provided"
+        "**Sending Images**\nUse `$image (prompt) (model) (width) (height) (enhancetext)` to generate an image.\n\n**Available Models** \n- pro (flux-pro) \n- realism (flux-realism) \n- anime (flux-anime) \n- 3D (flux-3D) \n- cablyai (flux-CablyAI) \n- turbo (turbo) \n\n**Optional Parameters** \n- Width: default 1024\n- Height: default 1024\n- Enhancetext: true if provided"
       );
     }
 
